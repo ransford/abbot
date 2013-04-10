8e9e9ca10fd017b6a9e7bb1a6142b83699a55a88
Ben Ransford <ben@ransford.org>
2013-04-10 17:28:51 -0400

    typos in tests/test.py

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,18 @@
-import abbott
+import abbot
 from twisted.words.xish import domish
 
 def makeMessage (msg):
     fakeMessage = domish.Element((None, 'message'))
     fakeMessage['from'] = 'ben@ransford.org'
-    fakeMessage['to'] = 'abbott@ransford.org'
+    fakeMessage['to'] = 'abbot@ransford.org'
     fakeMessage['type'] = 'chat'
     fakeMessage.addElement('body', content=str(msg))
     return fakeMessage
 
-mp = abbott.MessageParser()
+mp = abbot.MessageParser()
 (verb, args) = mp.parseString('help')
 
-ap = abbott.AbbottProtocol()
+ap = abbot.AbbotProtocol()
 m = makeMessage(verb)
 print "Message: ", m
 rep = ap.processMessage(m)
